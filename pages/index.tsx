c5b4d1debd3ef1cf0f1c1f5bc60e1a24f38660f9
Andres Rodriguez <arodriguezesc@hugeinc.com>
2022-06-15 08:36:07 -0500

    add plantCollection and author in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,26 @@
 import { GetStaticProps, InferGetStaticPropsType } from "Next";
-import { useEffect, useState } from "react";
 
 import { Layout } from '@components/Layout'
-import { getPlantList } from "@api";
+import { getPlantList, getAuthorList } from "@api";
 
 import { PlantCollection } from "@components/PlantCollection";
+import { Authors } from "@components/Authors";
 
 export const getStaticProps: GetStaticProps = async () => {
-  const plants = await getPlantList({limit: 10})
-  console.log(plants)
+  const plants = await getPlantList({limit: 20})
+  const authors = await getAuthorList({limit: 20})
   return {
     props: {
-      plants
+      plants,
+      authors
     }
   }
 }
 
-export default function Home({ plants }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home({ plants, authors }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Layout>
+      <Authors authors={authors} className="mb-10"/>
       <PlantCollection plants={plants} variant="square"/>
     </Layout>
   )
